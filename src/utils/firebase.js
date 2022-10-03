04981076d71e66c31f8a8ff3d406561fade61b87
JMStewart00 <jmstewart00@gmail.com>
2022-10-03 17:18:46 -0400

    Firebase Init: Github actions, deployments to firebase.

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore/lite';
+// import "dotenv";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "aberration-co.firebaseapp.com",
+    projectId: "aberration-co",
+    storageBucket: "aberration-co.appspot.com",
+    messagingSenderId: "870017547575",
+    appId: "1:870017547575:web:e0336884e3ef55bdaeaeb2",
+    measurementId: "G-75KC780ZLH"
+};
 
 // Use this to initialize the firebase App
 const firebaseApp = initializeApp(firebaseConfig);
